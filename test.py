284737e0acbab7ea7a6eda37bae358086cd8c359
vinhisreal <vinhquang2610345@gmail.com>
2025-01-04 08:35:30 +0000

    Jan 4, 2025, 3:35 PM

--- test.py
+++ test.py
@@ -331,23 +331,6 @@ def parse_grid(path):
 def output(a):
     sys.stdout.write(str(a))
 
-def display_sudoku(sudoku):
-    for i in range(9):
-        for j in range(9):
-            cell = sudoku[i][j]
-            if cell == 0 or isinstance(cell, set):
-                output('.')
-            else:
-                output(cell)
-            if (j + 1) % 3 == 0 and j < 8:
-                output(' |')
-
-            if j != 8:
-                output('  ')
-        output('\n')
-        if (i + 1) % 3 == 0 and i < 8:
-            output("--------+----------+---------\n")
-
 
 def extract_sudoku(image_path):
     final_image = parse_grid(image_path)
